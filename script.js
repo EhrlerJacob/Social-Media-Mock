617a857db592e8740e358b04f2761f2c8b775787
MAC <mac@MacBook-Pro.lan>
2023-03-29 23:01:33 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var count = 9
+var countElement = document.querySelector("#count");
+
+function add1() {
+    count++;
+    countElement.innerText = count + " like(s)"
+}
+
+var count2 = 12
+var countElement2 = document.querySelector("#count2")
+
+function add2() {
+    count2++;
+    countElement2.innerText = count2 + " like(s)"
+}
+
+var count3 = 9
+var countElement3 = document.querySelector("#count3");
+
+function add3() {
+    count3++;
+    countElement3.innerText = count3 + " like(s)";
+}
